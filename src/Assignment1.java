02b64a7d88ffe489376d87f76715861cea61a153
rjdaw <daw19003@byui.edu>
2021-07-26 13:33:26 -0600

    Finishing Assignment1

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -6,15 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Assignment1 {
 
     public static void main(String[] args) {
-        //Selenium code
-
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\32485\\Documents\\ContractPal\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(); //Create Driver object
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         WebElement checkbox = driver.findElement(By.xpath("//*[@id='checkBoxOption1']"));
         checkbox.click();
         System.out.println(driver.findElement(By.xpath("//*[@id='checkBoxOption1']")).isSelected());
-
-        //System.out.println(driver.getTitle());
     }
 }
